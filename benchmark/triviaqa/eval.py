6b30848f520133e9dfd1ff9874d5ab825d040c30
Muqi Li <muqi1029@gmail.com>
2025-04-15 21:57:57 +0800

    Add benchmark

---
+++ benchmark/triviaqa/eval.py
@@ -0,0 +1,68 @@
+import json
+
+import hydra
+from sglang import Runtime, assistant, function, gen, set_default_backend, user
+from tqdm import tqdm
+from utils import judge, load_dataset_util, second_prompt
+
+
+@function
+def chat(s, q):
+    s += user(q)
+    s += assistant(gen(name="answer", return_logprob=True))
+
+    logprobs = sum(
+        [tok[0] for tok in s.get_meta_info("answer")["output_token_logprobs"]]
+    )
+
+    s += user(second_prompt.format(avg_logprobs=logprobs))
+    s += assistant(gen(name="confidence", max_tokens=32))
+
+
+@hydra.main(config_path="triviaqa", config_name="config")
+def main(cfg):
+    # load dataset
+    dataset = load_dataset_util("trivia_qa")
+
+    set_default_backend(Runtime(model_path=cfg.model_path))
+
+    # batch_run
+    preds = []
+    answers = []
+    confidences = []
+
+    batch_size = cfg.batch_size
+    for i in tqdm(range(0, len(dataset), batch_size)):
+        start_idx = batch_size * i
+        end_idx = min(start_idx + batch_size, len(dataset))
+        states = chat.run_batch(
+            [{"q": dataset[i]["question"]} for i in range(start_idx, end_idx)],
+            max_new_tokens=cfg.max_new_tokens,
+        )
+        preds.extend([s["answer"] for s in states])
+        confidences.extend([s["confidence"] for s in states])
+        answers.extend(
+            [dataset[i]["answer"]["text"] for i in range(start_idx, end_idx)]
+        )
+
+    # compute acc
+    acc = sum([judge(p, a) for p, a in zip(preds, answers)]) / len(preds)
+    print(f"Accuracy: {acc}")
+
+    # compute uncertainty
+    # confidences = [float(u) for u in confidences]
+    # print(f"Average uncertainty: {sum(confidences) / len(confidences)}")
+    with open("triviaqa_results.json", "w") as f:
+        json.dump(
+            {
+                "preds": preds,
+                "answers": answers,
+                "confidences": confidences,
+                "acc": acc,
+            },
+            f,
+        )
+
+
+if __name__ == "__main__":
+    main()
---
+++ benchmark/utils.py
@@ -0,0 +1,19 @@
+from datasets import load_dataset
+
+second_prompt = """
+This is the logprobs of the first response:
+{avg_logprobs}, lower means the response is more creative or has potential of dangerous, please provide the confidence in the answer only in percent (0–100 %) direcly, then following the rationales:
+"""
+
+
+def load_dataset_util(dataset_name: str):
+    if dataset_name == "coqa":
+        return load_dataset("coqa", split="train")
+    elif dataset_name == "trivia_qa":
+        return load_dataset("TimoImhof/TriviaQA-in-SQuAD-format", name="unmodified")
+    else:
+        raise ValueError(f"Dataset {dataset_name} not found")
+
+
+def judge(pred, answer):
+    return answer in pred.lower()
